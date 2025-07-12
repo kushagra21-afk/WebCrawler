ad0b4a4b984092c5a78a7e1f2363ffdb440cf4e8
Kushagra Singhal <singhalkushagra21@gmail.com>
2025-07-13 01:17:02 +0530

    adding database and fixing code

--- robot.py
+++ robot.py
@@ -1,12 +1,10 @@
 import time
-from crawler import crawl
+from crawler import Crawler
 def start_crawling():
     while True:
         time.sleep(60) 
         try:
-            # Simulate crawling process
             print("Crawling in progress...")
-            crawl()
         except Exception as e:
             print(f"Error during crawling: {e}")
-            break  # Exit the loop on error
+            break  
